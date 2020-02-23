ef9307e256d1d830c142e93c441ae8735acad431
Mattèo Gauthier <matteo.gauthier@gmail.com>
2020-02-23 23:12:10 +0100

    Adding base file

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import UseOpenData from "./components/useOpenData";
+
+export default function App() {
+  return (
+    <div className="App">
+      <UseOpenData />
+    </div>
+  );
+}
