80871ba94769346a7008312bdb6e4c1143e591a3
Mattèo Gauthier <matteo.gauthier@gmail.com>
2020-02-24 12:43:24 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
-import "./styles.css";
-import UseOpenData from "./components/useOpenData";
+import React, { useEffect } from "react"
+import useSWR from "swr"
+import fetch from './libs/fetch'
 
 export default function App() {
+  const url =
+    "https://data.grandpoitiers.fr/api/records/1.0/search/?dataset=mobilites-stationnement-des-parkings-en-temps-reel&facet=Places_restantes"
+  const { data, error } = useSWR(url, fetch)
+  // useEffect(() => console.log(data), [])
   return (
     <div className="App">
-      <UseOpenData />
+
+      {JSON.stringify(data.records)}
     </div>
-  );
+  )
 }
+
+      {
+        /* {data.map(({ datasetid, fields }) => (
+        <li key={`photo-${datasetid}`}>
+          {fields}
+        </li>
+      ))} */
+      }
